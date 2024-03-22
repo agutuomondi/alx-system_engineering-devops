ede6b7efba7032a1f4a6f1d2d0e65c91206fa7e9
agutuomondi <mwalimuagutu@gmail.com>
2024-03-22 11:34:42 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill -f "killmenow"',
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep -f "killmenow"',
-}
+# kill process killmenow
 
+exec { 'pkill':
+
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
