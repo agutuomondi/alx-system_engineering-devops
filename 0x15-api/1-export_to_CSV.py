c38464b0283585ed39ab7336f22e6df1bd858046
agutuomondi <mwalimuagutu@gmail.com>
2024-04-24 14:34:27 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,31 +12,20 @@ import csv
 import requests
 import sys
 
-def fetch_user_info(user_id):
-    url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    res = requests.get(url_user)
-    user_name = res.json().get('username')
-    return user_name
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    task = url_user + '/todos'
 
-def fetch_user_tasks(user_id):
-    url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    task_url = f'{url_user}/todos'
-    res = requests.get(task_url)
-    tasks = res.json()
-    return tasks
+    res_user = requests.get(url_user)
+    user_name = res_user.json().get('username')
 
-def write_to_csv(user_id, user_name, tasks):
-    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    res_tasks = requests.get(task)
+    tasks = res_tasks.json()
+
+    with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:
             completed = task.get('completed')
             title_task = task.get('title')
-            csv_writer.writerow([user_id, user_name, completed, title_task])
-
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    user_name = fetch_user_info(user_id)
-    tasks = fetch_user_tasks(user_id)
-    write_to_csv(user_id, user_name, tasks)
-
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
