88c1c1f75958eb7bd0ae9e42528a430d97763194
agutuomondi <mwalimuagutu@gmail.com>
2024-04-24 14:03:08 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,28 +9,22 @@ Requirements:
 - Display progress information in the specified format
 '''
 
-import re
 import requests
 import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        id = int(sys.argv[1])
+        req = requests.get(f"{REST_API}/users/{id}").json()
+        task_req = requests.get(f"{REST_API}/todos").json()
+        emp_name = req.get('name')
+        tasks = [task for task in task_req if task.get('userId') == id]
+        completed_tasks = [task for task in tasks if task.get('completed')]
+        print(
+            f"Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):"
+        )
+        for task in completed_tasks:
+            print(f"\t {task.get('title')}")
+
