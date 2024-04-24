6db9be55105f84bd306682c4ac07ed732ee0720a
agutuomondi <mwalimuagutu@gmail.com>
2024-04-24 14:11:18 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,15 +12,20 @@ import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
+def fetch_user_info(user_id):
     url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     res = requests.get(url_user)
     user_name = res.json().get('username')
+    return user_name
+
+def fetch_user_tasks(user_id):
+    url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     task_url = f'{url_user}/todos'
     res = requests.get(task_url)
     tasks = res.json()
+    return tasks
 
+def write_to_csv(user_id, user_name, tasks):
     with open(f'{user_id}.csv', 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
@@ -28,4 +33,10 @@ if __name__ == '__main__':
             completed = task.get('completed')
             title_task = task.get('title')
             csv_writer.writerow([user_id, user_name, completed, title_task])
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    user_name = fetch_user_info(user_id)
+    tasks = fetch_user_tasks(user_id)
+    write_to_csv(user_id, user_name, tasks)
 
