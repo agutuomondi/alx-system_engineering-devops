8884703cae151ad1c3681aa10ee23d2a8febec26
agutuomondi <mwalimuagutu@gmail.com>
2024-05-09 11:44:50 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""
-Using Reddit's API
-"""
+"""Contains recurse function"""
 import requests
 
-def recurse(subreddit, hot_list=None, after=None):
-    """
-    Return top ten post titles recursively from a subreddit.
-    """
-    if hot_list is None:
-        hot_list = []
 
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {'after': after} if after else {}
-
-    try:
-        response = requests.get(url, params=params, headers=user_agent, allow_redirects=False)
-        response.raise_for_status()  # Raises an error for bad response
-
-        data = response.json().get("data")
-        after_data = data.get("after")
-
-        if after_data:
-            recurse(subreddit, hot_list, after=after_data)
-
-        all_titles = data.get("children")
-        for title_ in all_titles:
-            hot_list.append(title_["data"]["title"])
-
-        return hot_list
-
-    except requests.RequestException:
+def recurse(subreddit, hot_list=[], after="", count=0):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
