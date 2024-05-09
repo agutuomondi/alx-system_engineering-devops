e758323f20b2de6e3511a90e56d67d772083b812
agutuomondi <mwalimuagutu@gmail.com>
2024-05-09 11:39:54 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
+"""function that queries the Reddit API recursively."""
 
 
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_dict=None):
+def count_words(subreddit, word_list, after='', word_dict={}):
+    """ count all numers
     """
-    count all words
-    """
-    if word_dict is None:
-        word_dict = {word.lower(): 0 for word in word_list}
+
+    if not word_dict:
+        for word in word_list:
+            if word.lower() not in word_dict:
+                word_dict[word.lower()] = 0
 
     if after is None:
-        word_dict_sorted = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in word_dict_sorted:
-            if count:
-                print('{}: {}'.format(word, count))
+        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
         return None
 
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
@@ -33,10 +35,12 @@ def count_words(subreddit, word_list, after='', word_dict=None):
         aft = response.json()['data']['after']
         for post in hot:
             title = post['data']['title']
-            lower = [word.lower() for word in title.split()]
-            for word in word_dict:
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in word_dict.keys():
                 word_dict[word] += lower.count(word)
 
     except Exception:
         return None
 
+    count_words(subreddit, word_list, aft, word_dict)
